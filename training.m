e9b06ca01a784501139c134d1ed6598a65368c64
Jacob Laufer <36800304+skooba@users.noreply.github.com>
2019-04-08 21:16:39 -0400

    Update training.m
    Change number in line 3 (number is .23) and line 48 (number is 1.2) to optimize

--- training.m
+++ training.m
@@ -1,8 +1,9 @@
-  %% Compute the kernel function
+ 
+%% Compute the kernel function
+    load MNIST_data
     [n,m] = size(train_samples);
     %sigma = sum(var(train_samples));
-    %load K
-    K = gaussian_kernel(n,n,train_samples,train_samples,.05);
+    K = gaussian_kernel(n,n,train_samples,train_samples,.23);
 
     %% One-versus-rest algorithim 
     %Maximze the dual formulation of the SVM 
@@ -13,7 +14,7 @@
         idx =find(train_samples_labels == label);
         ONEvsREST = -ones(n,1);
         ONEvsREST(idx) = 1;
-        a = SVM(n,ONEvsREST,K);
+        a = SVM(n,ONEvsREST,K, 1);
         a_matrix(:,label+1) = a;
     %find value of threshold parameter
         support_vectors_index = find(a > .00001);
@@ -28,8 +29,6 @@
     %% One-versus-one algorithim
     %initialize target vector with +1 and -1 for the two class SVM and 0 for
     %the other 8 classes
-%    output_matrixOvO = zeros(n,10); %one-versus-one algorthim trains N(N-1)/2 SVMS
-%    output_matrixOvO_delete = zeros(n,10); %will use for DAGSVM
     SVMnum = 0;
     skip = 0;
     bOvO_vector = zeros(45,1)
@@ -40,23 +39,22 @@
         j = 1 + skip; %modify j so we do not repeat any SVMs
         skip = skip + 1;
         while j <= 9
-            SVMnum = SVMnum + 1; %count the number SVM we are on
+            SVMnum = SVMnum + 1 %count the number SVM we are on
             idx_pos = find(train_samples_labels == i);
-            %length(idx_pos);
             idx_neg = find(train_samples_labels == j); 
-            %length(idx_neg)
             ONEvsONE = zeros(n,1);
             ONEvsONE(idx_pos) = 1;
             ONEvsONE(idx_neg) = -1;
-            aOvO = SVM(n,ONEvsONE,K);
+            aOvO = SVM(n,ONEvsONE,K,1.2);
             aOvO_matrix(:,SVMnum) = aOvO;
-            support_vectors_indexOvO = find(aOvO > .0000001);
-            [~,SV_lengthOvO] = size(support_vectors_indexOvO');
+            support_vectors_indexOvO = find(aOvO > .00001);
+            SV_lengthOvO = length(support_vectors_indexOvO);
             bOvO = threshold(SV_lengthOvO, ONEvsONE,aOvO, support_vectors_indexOvO,K);
             bOvO_vector(SVMnum) = bOvO; 
             yOvO = classify(n, support_vectors_indexOvO, ONEvsONE, aOvO, K, bOvO);   
             yOvO_matrix(:,SVMnum) = yOvO;
-            column_names = [column_names, i,j]; % keep track of column names for DAGSVM
+            column_names = [column_names, strcat(num2str(i),num2str(j))]; % keep track of column names for DAGSVM
             j = j + 1;
         end
     end
+
